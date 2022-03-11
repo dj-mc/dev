5441c9446d5e48760dbdbde6d21723fbc3f074a1
dj-mc <49037253+dj-mc@users.noreply.github.com>
2022-03-11 16:37:06 -0600

    Bolster folder commands

--- setup.sh
+++ setup.sh
@@ -5,8 +5,8 @@ printf "%s" "$key"
 
 sudo apt install git stow
 
-mkdir ~/d; cd ~/d; git clone https://github.com/dj-mc/dev
+mkdir -p ~/d; cd ~/d || exit; git clone https://github.com/dj-mc/dev
 
-cd ~
+cd ~ || exit
 git clone "https://$key@github.com/dj-mc/.secrets.git"
 git clone https://github.com/dj-mc/.dotfiles.git

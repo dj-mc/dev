3dcfe4b982422ae9394f81378572d718e2d767d7
dj-mc <49037253+dj-mc@users.noreply.github.com>
2022-12-14 08:39:04 -0600

    Add playground scripts

--- playground/play-00.sh
+++ playground/play-00.sh
@@ -1,39 +1,98 @@
 #!/usr/bin/env bash
 
-cat /etc/shells
-cat /etc/passwd | grep dan
+function list_shells() {
+    cat /etc/shells
+    cat /etc/passwd | grep dan
+}
 
-# Invocations
-# An interactive shell reads/writes to a user's terminal
-# Interactive login shell
-cat ~/.profile
-cat ~/.bash_logout
-cat /etc/profile
+function interactive_shell() {
+    # Invocations
+    # An interactive shell reads/writes to a user's terminal
 
-# Invoked with `sh`
-# Same as above, but without ~/.bash_logout
+    # Interactive login shell
+    # Reads from the following files
+    cat ~/.profile
+    cat ~/.bash_logout
+    cat /etc/profile
 
-# Interactive non-login shell
-cat ~/.bashrc
+    # When invoked with `sh`
+    # Same as above, but without ~/.bash_logout
+}
 
-# Non-interactive
-# All scripts use non-interactive shells
-echo $BASH_ENV
+function non_interactive() {
+    # Interactive non-login shell
+    # Only reads from
+    cat ~/.bashrc
 
-# Is it interactive?
-echo $-
+    # All scripts use non-interactive shells
+    # Non-interactive shells read from:
+    # echo "$BASH_ENV"
+}
 
-# POSIX mode
-# set -o posix
-echo $ENV
+function check_interactive() {
+    # Is it interactive?
+    echo $-
+}
 
-# Directory stack
-dirs
+function posix_mode() {
+    # Set POSIX mode
+    # set -o posix
 
-pushd ~
-pushd ..
-pushd /etc
-dirs
+    # Check if in POSIX mode
+    set -o | grep posix
 
-popd ~
-dirs
+    # POSIX mode shells read from:
+    # echo "$ENV"
+}
+
+function directory_stack() {
+    # Directory stack
+    pushd ~ || exit
+    pushd /etc || exit
+    pushd ..
+    popd || exit
+    popd || exit
+    popd || exit
+}
+
+# Attempts at getting function names from this file:
+
+# grep "^function" "$0"
+
+# typeset -f | awk '/ \(\) / {print $1}'
+
+# typeset -f | awk '/ \(\) $/ && !/^main / {print $1}'
+
+# declare -F | awk '{print $NF}'
+
+# Alphabetical order, not declared order
+# for func in $(declare -F | awk '{print $3}'); do
+#     clear
+#     ${func}
+#     printf "\nEnd of function call: %s\n" "${func}"
+#     read -n1 -s -r -p "Press a key to continue"
+#     printf "\n"
+#     clear
+# done
+
+declare -a execute_these=(
+    list_shells
+    interactive_shell
+    non_interactive
+    check_interactive
+    posix_mode
+    directory_stack
+)
+
+function main() {
+    for func in "${execute_these[@]}"; do
+        clear
+        ${func}
+        printf "\nEnd of function call: %s\n" "${func}"
+        read -n1 -s -r -p "Press a key to continue"
+        printf "\n"
+        clear
+    done
+}
+
+main
